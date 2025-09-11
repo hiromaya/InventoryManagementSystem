a13255410055108f70c15281e2b313b0f650e712
hiromaya <info@aberkam.com>
2025-09-12 06:00:33 +0900

    fix(db): make 06_add_department_columns.sql idempotent (IF COL_LENGTH/IF NOT EXISTS for columns and indexes)

--- database/06_add_department_columns.sql
+++ database/06_add_department_columns.sql
@@ -2,24 +2,52 @@
 -- 実行日: 2025-06-24
 
 -- 部門コードカラムの追加
-ALTER TABLE DataSets ADD DepartmentCode NVARCHAR(10) NOT NULL DEFAULT 'DeptA';
-ALTER TABLE SalesVouchers ADD DepartmentCode NVARCHAR(10) NOT NULL DEFAULT 'DeptA';
-ALTER TABLE PurchaseVouchers ADD DepartmentCode NVARCHAR(10) NOT NULL DEFAULT 'DeptA';
-ALTER TABLE InventoryAdjustments ADD DepartmentCode NVARCHAR(10) NOT NULL DEFAULT 'DeptA';
-ALTER TABLE CpInventoryMaster ADD DepartmentCode NVARCHAR(10) NOT NULL DEFAULT 'DeptA';
+IF COL_LENGTH('DataSets', 'DepartmentCode') IS NULL
+    ALTER TABLE DataSets ADD DepartmentCode NVARCHAR(10) NOT NULL DEFAULT 'DeptA';
+GO
+IF COL_LENGTH('SalesVouchers', 'DepartmentCode') IS NULL
+    ALTER TABLE SalesVouchers ADD DepartmentCode NVARCHAR(10) NOT NULL DEFAULT 'DeptA';
+GO
+IF COL_LENGTH('PurchaseVouchers', 'DepartmentCode') IS NULL
+    ALTER TABLE PurchaseVouchers ADD DepartmentCode NVARCHAR(10) NOT NULL DEFAULT 'DeptA';
+GO
+IF COL_LENGTH('InventoryAdjustments', 'DepartmentCode') IS NULL
+    ALTER TABLE InventoryAdjustments ADD DepartmentCode NVARCHAR(10) NOT NULL DEFAULT 'DeptA';
+GO
+IF COL_LENGTH('CpInventoryMaster', 'DepartmentCode') IS NULL
+    ALTER TABLE CpInventoryMaster ADD DepartmentCode NVARCHAR(10) NOT NULL DEFAULT 'DeptA';
+GO
 
 -- インデックスの追加（検索性能向上）
-CREATE INDEX IX_DataSets_DepartmentCode ON DataSets(DepartmentCode);
-CREATE INDEX IX_SalesVouchers_DepartmentCode ON SalesVouchers(DepartmentCode);
-CREATE INDEX IX_PurchaseVouchers_DepartmentCode ON PurchaseVouchers(DepartmentCode);
-CREATE INDEX IX_InventoryAdjustments_DepartmentCode ON InventoryAdjustments(DepartmentCode);
-CREATE INDEX IX_CpInventoryMaster_DepartmentCode ON CpInventoryMaster(DepartmentCode);
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = 'IX_DataSets_DepartmentCode' AND object_id = OBJECT_ID('DataSets'))
+    CREATE INDEX IX_DataSets_DepartmentCode ON DataSets(DepartmentCode);
+GO
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = 'IX_SalesVouchers_DepartmentCode' AND object_id = OBJECT_ID('SalesVouchers'))
+    CREATE INDEX IX_SalesVouchers_DepartmentCode ON SalesVouchers(DepartmentCode);
+GO
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = 'IX_PurchaseVouchers_DepartmentCode' AND object_id = OBJECT_ID('PurchaseVouchers'))
+    CREATE INDEX IX_PurchaseVouchers_DepartmentCode ON PurchaseVouchers(DepartmentCode);
+GO
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = 'IX_InventoryAdjustments_DepartmentCode' AND object_id = OBJECT_ID('InventoryAdjustments'))
+    CREATE INDEX IX_InventoryAdjustments_DepartmentCode ON InventoryAdjustments(DepartmentCode);
+GO
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = 'IX_CpInventoryMaster_DepartmentCode' AND object_id = OBJECT_ID('CpInventoryMaster'))
+    CREATE INDEX IX_CpInventoryMaster_DepartmentCode ON CpInventoryMaster(DepartmentCode);
+GO
 
 -- 複合インデックス（部門とジョブデートでの検索用）
-CREATE INDEX IX_DataSets_DepartmentCode_JobDate ON DataSets(DepartmentCode, JobDate);
-CREATE INDEX IX_SalesVouchers_DepartmentCode_JobDate ON SalesVouchers(DepartmentCode, JobDate);
-CREATE INDEX IX_PurchaseVouchers_DepartmentCode_JobDate ON PurchaseVouchers(DepartmentCode, JobDate);
-CREATE INDEX IX_InventoryAdjustments_DepartmentCode_JobDate ON InventoryAdjustments(DepartmentCode, JobDate);
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = 'IX_DataSets_DepartmentCode_JobDate' AND object_id = OBJECT_ID('DataSets'))
+    CREATE INDEX IX_DataSets_DepartmentCode_JobDate ON DataSets(DepartmentCode, JobDate);
+GO
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = 'IX_SalesVouchers_DepartmentCode_JobDate' AND object_id = OBJECT_ID('SalesVouchers'))
+    CREATE INDEX IX_SalesVouchers_DepartmentCode_JobDate ON SalesVouchers(DepartmentCode, JobDate);
+GO
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = 'IX_PurchaseVouchers_DepartmentCode_JobDate' AND object_id = OBJECT_ID('PurchaseVouchers'))
+    CREATE INDEX IX_PurchaseVouchers_DepartmentCode_JobDate ON PurchaseVouchers(DepartmentCode, JobDate);
+GO
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = 'IX_InventoryAdjustments_DepartmentCode_JobDate' AND object_id = OBJECT_ID('InventoryAdjustments'))
+    CREATE INDEX IX_InventoryAdjustments_DepartmentCode_JobDate ON InventoryAdjustments(DepartmentCode, JobDate);
+GO
 
 -- 部門マスタテーブルの作成（将来の拡張用）
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[DepartmentMaster]') AND type in (N'U'))
